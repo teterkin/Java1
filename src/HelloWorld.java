bf3e10d73d983ce4af3389a919d8d52bddf6b5cb
Teterkin Alexander <a.teterkin@hostco.ru>
2017-11-21 23:38:34 +0500

    Warnings included as comments

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
+        /*
+            Warnings:
+                Warning:(14, 28) Value 'f' is always 'true'
+                Warning:(15, 13) Condition 'd<10' is always 'false'
+                Warning:(23, 16) Access can be private
+         */
         int b;
         int a = 5;
         b = a + 2;

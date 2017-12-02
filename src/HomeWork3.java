cb2424e019b81d3f282226eff864b3840ba315e4
Teterkin Alexander <a.teterkin@hostco.ru>
2017-12-02 16:27:13 +0500

    HomeWork3: 2 Tasks Done. No file read support for 2nd task yet.

--- src/HomeWork3.java
+++ src/HomeWork3.java
@@ -1,5 +1,12 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class HomeWork3
 {
+    static Random random = new Random();
+    static Scanner sc = new Scanner(System.in);
+    
     public static void main(String[] args)
     {
         /*
@@ -11,6 +18,32 @@ public class HomeWork3
         После победы или проигрыша выводится запрос – «Повторить игру еще раз?
         1 – да / 0 – нет»(1 – повторить, 0 – нет).
          */
+        System.out.println("\nДобро пожаловать в игру УГАДАЙКА!");
+        boolean wannaPlay = true;
+        Scanner scanner = new Scanner(System.in);
+        while (wannaPlay)
+        {
+            int won = guessGame();
+            switch (won)
+            {
+                case 0:
+                    System.out.println("Ты выиграл! :)");
+                    break;
+                case 1:
+                    System.out.println("Жаль. Ты проиграл. :(");
+                    break;
+                default:
+                    System.out.println("Something went wrong! :-|");
+            }
+            System.out.println("\nСыграем ещё? 1 – да / 0 – нет");
+            int yesNo = scanner.nextInt();
+            if (yesNo == 0)
+            {
+                wannaPlay = false;
+                System.out.printf("Пока!");
+            }
+        }
+        
         
         /*
         TODO: Задача №2 (Повышенной сложности) - Сделать чтение массива из файла.
@@ -30,5 +63,63 @@ public class HomeWork3
         Играем до тех пор, пока игрок не отгадает слово
         Используем только маленькие буквы
          */
+        System.out.printf("\nБонусная игра! Спасибо biblelamp за развлечение!\n");
+        guessTheWord();
+        scanner.close();
+    }
+    
+    private static int guessGame()
+    {
+        int won = -1; // something else
+        int number = (int) (Math.random() * 10);
+        // DEBUG
+        //System.out.println(number);
+        System.out.println("\nЯ загадал число от 0 до 9. Угадай его! У тебя только 3 попытки:\n");
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 3; i++)
+        {
+            System.out.print("Попытка № " + i + " => ");
+            int guess = scanner.nextInt();
+            if (guess == number)
+            {
+                won = 0;
+                System.out.println("Уррра!!!");
+                break;
+            } else
+            {
+                won = 1;
+                if (guess < number)
+                {
+                    System.out.println("Нет. Моё чуть побольше...");
+                }
+                else
+                {
+                    System.out.println("Не. Моё чуть поменьше...");
+                }
+            }
+        }
+        return won; //change
+    }
+    
+    static void guessTheWord() {
+        String[] words = {
+                "apple", "orange", "lemon", "banana", "apricot",
+                "avocado", "broccoli", "carrot", "cherry", "garlic",
+                "grape", "melon", "leak", "kiwi", "mango",
+                "mushroom", "nut", "olive", "pea", "peanut",
+                "pear", "pepper", "pineapple", "pumpkin", "potato"};
+        String guess = null;
+        String word = words[random.nextInt(words.length)];
+        System.out.println(Arrays.toString(words));
+        do {
+            System.out.print("Guess the word: ");
+            guess = sc.next();
+            for (int i = 0; i < 15; i++)
+                System.out.print(
+                        (i < word.length() && i < guess.length()
+                                && word.charAt(i) == guess.charAt(i))?
+                                word.charAt(i) : ((word.equals(guess))? "" : "#"));
+            System.out.println();
+        } while (!word.equals(guess));
     }
 }

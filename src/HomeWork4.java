351dd67a9363a79350ed4919f1be0167876811dd
Teterkin Alexander <a.teterkin@hostco.ru>
2017-12-02 20:27:28 +0500

    HomeWork4: Initial Commit. Game ir Ready. It uses for cycles to count the lines, but AI does not have an intelligence yet.

---
+++ src/HomeWork4.java
@@ -0,0 +1,273 @@
+import java.util.Scanner;
+
+/**
+ * Домашнее задание №4
+ * Игра Крестики-Нолики
+ * Начинаем понемногу осваивать обектно-ориентированные методы.
+ * Еще пока не на полную мощь.
+ *
+ * @author teterkin@gmail.com
+ * @version 02/12/2017
+ * @link https://github.com/teterkin/Java1.git
+ */
+public class HomeWork4
+{
+    final int mySIZE = 3;
+    final int myTreshold = 3;
+    final char myX = 'x';
+    final char myO = 'o';
+    final char myEmpty = '.';
+    char[][] myMap = new char[mySIZE][mySIZE];
+    Boolean Done = false;
+    Scanner scanner = new Scanner(System.in);
+    
+    /**
+     * Запуск главного метода класса.
+     * Он инициализирует класс с помощью конструктора и запускает основной цикл игры.
+     * По завершении работы основного цикла метод прощается с игроком.
+     *
+     * @param args Аргументы командной строки не используются.
+     */
+    public static void main(String[] args)
+    {
+        HomeWork4 hw = new HomeWork4();
+        hw.run();
+        System.out.println("Пока!");
+        
+    }
+    
+    /**
+     * Конструктор класса. Инициализирует наш объект.
+     */
+    HomeWork4()
+    {
+        System.out.println("Игра началась!");
+        initMap();
+        displayMap();
+    }
+    
+    /**
+     * Инициализируем игровое поле.
+     */
+    private void initMap()
+    {
+        System.out.println("Подготавливаю крестики и нолики...");
+        for (int i = 0; i < mySIZE; i++)
+        {
+            for (int j = 0; j < mySIZE; j++)
+            {
+                myMap[i][j] = '.';
+            }
+        }
+        System.out.println("Готово.");
+    }
+    
+    /**
+     * Отображение игрового поля
+     */
+    private void displayMap()
+    {
+        System.out.println("  1 2 3");
+        for (int i = 0; i < mySIZE; i++)
+        {
+            System.out.print((i+1) + " ");
+            for (int j = 0; j < mySIZE; j++)
+            {
+                System.out.print(myMap[i][j]+ " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+    
+    /**
+     * Основной цикл игры
+     */
+    private void run()
+    {
+        while (!Done)
+        {
+            playerTurn();
+            displayMap();
+            if (checkIfWin(myX))
+            {
+                System.out.println("Ты выиграл! Уррра!!!");
+                Done = true;
+            }
+            else
+            {
+                if (!isFull())
+                {
+                    aiTurn();
+                    displayMap();
+                    if (checkIfWin(myO))
+                    {
+                        
+                        
+                        
+                        System.out.println("Искусственный интеллект выиграл! " +
+                                "Но ты приходи, если что... ;)");
+                        Done = true;
+                    }
+                    else
+                    {
+                        isFull();
+                    }
+                }
+            }
+        }
+    }
+    
+    /**
+     * Ход искусственного интеллекта
+     */
+    private void aiTurn()
+    {
+        boolean keepAsking = true;
+        System.out.println("Тэкс... ИИ думает...");
+        while (keepAsking)
+        {
+            int mapX = (int) (Math.random() * 3 + 1);
+            int mapY = (int) (Math.random() * 3 + 1);
+            if (isValid(mapX, mapY))
+            {
+                keepAsking = false;
+                myMap[mapX - 1][mapY - 1] = myO;
+                System.out.println("Он ходит: [" + (mapY) + "," + (mapX) + "]");
+                System.out.println();
+            }
+        }
+    }
+    
+    /**
+     * Проверка на заполненность игрового поля.
+     * Если так,  то игра заканчивается Ничьёй.
+     */
+    private boolean isFull()
+    {
+        boolean isFull = true;
+        for (int i = 0; i < mySIZE; i++)
+        {
+            for (int j = 0; j < mySIZE; j++)
+            {
+                if (myMap[i][j] == '.')
+                {
+                    isFull = false;
+                }
+            }
+        }
+        if (isFull)
+        {
+            Done = true;
+            System.out.println("Ничья!");
+        }
+        return isFull;
+    }
+    
+    /**
+     * Проверка выигрыша крестиков или ноликов.
+     *
+     * @param myChar В этой переменной указывается, что проверяем, крестики или нолики?
+     *               Мы используем константы определенные в начале класса.
+     */
+    private boolean checkIfWin(char myChar)
+    {
+        /*
+            Должен быть один цикл который просто делает 3 складывания
+            При этом он должен сложить 8 переменных и в конце их все проверить, есть ли
+            линия.
+        }*/
+        int[] line = new int[8];
+        //сверху вниз
+        for (int i = 0; i < mySIZE; i++)
+        {
+            for (int j = 0; j < mySIZE; j++)
+            {
+                // Горизонтальная линия
+                if (myMap[i][j] == myChar)
+                {
+                    line[i]++;
+                }
+                // Вертикальная линия
+                if (myMap[j][i] == myChar)
+                {
+                    line[(i+3)]++;
+                }
+            }
+            // слева направо сверху вних
+            if (myMap[i][i] == myChar)
+            {
+                line[6]++;
+            }
+            // слева направо снизу вверх
+            if (myMap[i][2-i] == myChar)
+            {
+                line[7]++;
+            }
+        }
+        // System.out.println();
+        boolean didIWon = false;
+        for (int i = 0; i < line.length; i++)
+        {
+            //System.out.print(line[i]+" ");
+            if (line[i] >= myTreshold)
+            {
+                didIWon = true;
+            }
+        }
+        // System.out.println();
+        return didIWon;
+    }
+    
+    /**
+     * Ход игрока. запрашиваем ввод координат для крестика.
+     * Число от 1 до 3-х, как для X, так и для Y.
+     */
+    private void playerTurn()
+    {
+        boolean keepAsking = true;
+        while (keepAsking)
+        {
+            System.out.println("Введите координаты крестика (X,Y) через пробел.");
+            System.out.println("Обк координаты должны быть в пределах от 1 до " + mySIZE + ".");
+            System.out.print("=> ");
+            int mapX = 0;
+            int mapY = 0;
+            /*
+                Используем try-catch чтобы не вводили всякую ерунду.
+            */
+            try
+            {
+                mapY = scanner.nextInt();
+                mapX = scanner.nextInt();
+                if (isValid(mapX, mapY))
+                {
+                    keepAsking = false;
+                    myMap[mapX - 1][mapY - 1] = myX;
+                    System.out.println("\nТы сходил: [" + (mapY) + "," + (mapX) + "]");
+                    System.out.println();
+                } else
+                {
+                    System.out.println("Сюда крестик не поставишь :(");
+                    displayMap();
+                }
+            } catch (Exception e)
+            {
+                // e.printStackTrace();
+                System.out.println("Ошибочка вышла. Попробуй ещё!");
+                scanner.next(); // Это нужно чтобы не было бесконечного цикла с ошибкой
+                displayMap();
+            }
+        }
+    }
+    
+    private boolean isValid(int x, int y)
+    {
+        boolean isValid = false;
+        if (x > 0 && x <= mySIZE && y > 0 && y <= mySIZE && myMap[x - 1][y - 1] == '.')
+        {
+            isValid = true;
+        }
+        return isValid;
+    }
+}

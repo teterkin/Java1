340d1aad28e4f415c2e7dd068eaf27d3134be988
Teterkin Alexander <a.teterkin@hostco.ru>
2017-12-14 00:47:54 +0500

    HomeWork7: Console app is done.

--- src/HowmeWork7.java
+++ src/HowmeWork7.java
@@ -23,10 +23,50 @@ public class HowmeWork7
         
         System.out.println(plate);
         System.out.println(cat);
-        
-        cat.eat(plate);
     
+        System.out.println("\nБарсик поест 10 раз:");
+        // Tests for negative number of food amounts.
+        for (int i = 0; i < 10; i++)
+        {
+            cat.eat(plate);
+            System.out.println(plate);
+        }
+    
+        System.out.println("\nСоздадим 10 котов:");
+        String[] catNames = {"Ешка","Барсик","Кузя","Алекс","Кекс","Мурзик","Персик",
+                "Марсик","Лаки","Тишка"};
+        SmartCat[] cats = new SmartCat[10];
+        int idx = 0;
+        for (String nextCat:catNames)
+        {
+            //System.out.println(nextCat);
+            cats[idx] = new SmartCat(nextCat, (int)(Math.random()*6+5));
+            System.out.println(cats[idx]);
+            idx++;
+        }
+    
+        System.out.println("\nНаполним тарелку:");
+        plate.setFood(50);
         System.out.println(plate);
+    
+        System.out.println("\nНачнем по очереди кормить котов:");
+        System.out.println("Всего у нас " + idx + " котов.");
+        for (int i = 0; i < 10; i++)
+        {
+            boolean result = cats[i].eat(plate);
+            if (result)
+            {
+                cats[i].setHungry(false);
+            }
+            System.out.println(plate);
+        }
+        System.out.println("Надеюсь, всем хватило?");
+    
+        System.out.println("\nПосмотрим, сыты ли коты?");
+        for (int i = 0; i < 10; i++)
+        {
+            System.out.println(cats[i]);
+        }
     }
 }
 
@@ -34,11 +74,23 @@ class SmartCat
 {
     private String name;
     private int appetite;
+    private boolean hungry;
     
     public SmartCat(String name, int appetite)
     {
         this.name = name;
         this.appetite = appetite;
+        this.hungry = true;
+    }
+    
+    public boolean isHungry()
+    {
+        return hungry;
+    }
+    
+    public void setHungry(boolean hungry)
+    {
+        this.hungry = hungry;
     }
     
     public String getName()
@@ -61,10 +113,20 @@ class SmartCat
         this.appetite = appetite;
     }
     
-    void eat(Plate plate)
+    boolean eat(Plate plate)
     {
-        System.out.println(name + ": Ням-ням.");
-        plate.decreaseFood(appetite);
+        boolean RC = false;
+        if (plate.getFood() >= appetite)
+        {
+            System.out.println(name + ": Хрум-хрум.");
+            plate.decreaseFood(appetite);
+            RC = true;
+        }
+        else
+        {
+            System.out.println(name + ": Мяяяяу!!!");
+        }
+        return RC;
     }
     
     @Override
@@ -73,6 +135,7 @@ class SmartCat
         return "Кот {" +
                 " кличка = '" + name + '\'' +
                 ", аппетит = " + appetite +
+                ", голоден = " + hungry +
                 " }";
     }
 }
@@ -95,5 +158,15 @@ class Plate
     public String toString()
     {
         return "Тарелка { еда = " + food + " }";
+    }
+    
+    public int getFood()
+    {
+        return food;
+    }
+    
+    public void setFood(int food)
+    {
+        this.food = food;
     }
 }

d4c2ccdcf4763cf2b88447f9852dacd8a1926bc7
Teterkin Alexander <a.teterkin@hostco.ru>
2017-12-13 23:05:37 +0500

    HomeWork7: Minor modifications. Strings translated to Russian.

--- src/HowmeWork7.java
+++ src/HowmeWork7.java
@@ -9,11 +9,91 @@
  *  5. Создать массив котов и тарелку с едой, попросить всех котов покушать из этой тарелки
  *     и потом вывести информацию о сытости котов в консоль
  *  6. Добавить в тарелку метод, с помощью которого можно было бы добавлять еду в тарелку
+ *  На Exelent - графическое приложение про Котов.
  *  @author teterkin@gmail.com
  *  @version 20171213
  *  @link https://github.com/teterkin/Java1/blob/master/src/HowmeWork7.java
  */
 public class HowmeWork7
 {
+    public static void main(String[] args)
+    {
+        SmartCat cat = new SmartCat("Барсик", 10);
+        Plate plate = new Plate(50);
+        
+        System.out.println(plate);
+        System.out.println(cat);
+        
+        cat.eat(plate);
+    
+        System.out.println(plate);
+    }
+}
 
+class SmartCat
+{
+    private String name;
+    private int appetite;
+    
+    public SmartCat(String name, int appetite)
+    {
+        this.name = name;
+        this.appetite = appetite;
+    }
+    
+    public String getName()
+    {
+        return name;
+    }
+    
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    
+    public int getAppetite()
+    {
+        return appetite;
+    }
+    
+    public void setAppetite(int appetite)
+    {
+        this.appetite = appetite;
+    }
+    
+    void eat(Plate plate)
+    {
+        System.out.println(name + ": Ням-ням.");
+        plate.decreaseFood(appetite);
+    }
+    
+    @Override
+    public String toString()
+    {
+        return "Кот {" +
+                " кличка = '" + name + '\'' +
+                ", аппетит = " + appetite +
+                " }";
+    }
+}
+
+class Plate
+{
+    private int food;
+    
+    public Plate(int food)
+    {
+        this.food = food;
+    }
+    
+    void decreaseFood(int appetite)
+    {
+        this.food -= appetite;
+    }
+    
+    @Override
+    public String toString()
+    {
+        return "Тарелка { еда = " + food + " }";
+    }
 }

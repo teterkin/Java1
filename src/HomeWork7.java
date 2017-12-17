39475bc9dd9b460e579ea76a17cfd2ce89ea4727
teterkin <teterkin@gmail.com>
2017-12-17 23:02:38 +0500

    Renamed file for Home Work7 + added Project files.

--- src/HomeWork7.java
+++ src/HomeWork7.java
@@ -1,54 +1,55 @@
 /**
- *  1. Расширить задачу про котов и тарелки с едой
- *  2. Сделать так, чтобы в тарелке с едой не могло получиться отрицательного количества
- *  еды (например, в миске 10 еды, а кот пытается покушать 15-20)
- *  3. Каждому коту нужно добавить поле сытость (когда создаем котов, они голодны).
- *     Если коту удалось покушать (хватило еды), сытость = true
- *  4. Считаем, что если коту мало еды в тарелке, то он ее просто не трогает,
- *     то есть не может быть наполовину сыт (это сделано для упрощения логики программы)
- *  5. Создать массив котов и тарелку с едой, попросить всех котов покушать из этой тарелки
- *     и потом вывести информацию о сытости котов в консоль
- *  6. Добавить в тарелку метод, с помощью которого можно было бы добавлять еду в тарелку
- *  На Exelent - графическое приложение про Котов.
- *  @author teterkin@gmail.com
- *  @version 20171213
- *  @link https://github.com/teterkin/Java1/blob/master/src/HowmeWork7.java
+ * 1. Расширить задачу про котов и тарелки с едой
+ * 2. Сделать так, чтобы в тарелке с едой не могло получиться отрицательного количества
+ * еды (например, в миске 10 еды, а кот пытается покушать 15-20)
+ * 3. Каждому коту нужно добавить поле сытость (когда создаем котов, они голодны).
+ * Если коту удалось покушать (хватило еды), сытость = true
+ * 4. Считаем, что если коту мало еды в тарелке, то он ее просто не трогает,
+ * то есть не может быть наполовину сыт (это сделано для упрощения логики программы)
+ * 5. Создать массив котов и тарелку с едой, попросить всех котов покушать из этой тарелки
+ * и потом вывести информацию о сытости котов в консоль
+ * 6. Добавить в тарелку метод, с помощью которого можно было бы добавлять еду в тарелку
+ * На Exelent - графическое приложение про Котов.
+ *
+ * @author teterkin@gmail.com
+ * @version 20171213
+ * @link https://github.com/teterkin/Java1/blob/master/src/HowmeWork7.java
  */
-public class HowmeWork7
+public class HomeWork7
 {
     public static void main(String[] args)
     {
-        SmartCat cat = new SmartCat("Барсик", 10);
         Plate plate = new Plate(50);
-        
-        System.out.println(plate);
-        System.out.println(cat);
-    
-        System.out.println("\nБарсик поест 10 раз:");
-        // Tests for negative number of food amounts.
-        for (int i = 0; i < 10; i++)
-        {
-            cat.eat(plate);
-            System.out.println(plate);
-        }
-    
+//        SmartCat cat = new SmartCat("Барсик", 10);
+//
+//        System.out.println(plate);
+//        System.out.println(cat);
+//
+//        System.out.println("\nБарсик поест 10 раз:");
+//        // Tests for negative number of food amounts.
+//        for (int i = 0; i < 10; i++)
+//        {
+//            cat.eat(plate);
+//            System.out.println(plate);
+//        }
+
         System.out.println("\nСоздадим 10 котов:");
-        String[] catNames = {"Ешка","Барсик","Кузя","Алекс","Кекс","Мурзик","Персик",
-                "Марсик","Лаки","Тишка"};
+        String[] catNames = {"Ешка", "Барсик", "Кузя", "Алекс", "Кекс", "Мурзик", "Персик",
+                "Марсик", "Лаки", "Тишка"};
         SmartCat[] cats = new SmartCat[10];
         int idx = 0;
-        for (String nextCat:catNames)
+        for (String nextCat : catNames)
         {
             //System.out.println(nextCat);
-            cats[idx] = new SmartCat(nextCat, (int)(Math.random()*6+5));
+            cats[idx] = new SmartCat(nextCat, (int) (Math.random() * 6 + 5));
             System.out.println(cats[idx]);
             idx++;
         }
-    
+
         System.out.println("\nНаполним тарелку:");
         plate.setFood(50);
         System.out.println(plate);
-    
+
         System.out.println("\nНачнем по очереди кормить котов:");
         System.out.println("Всего у нас " + idx + " котов.");
         for (int i = 0; i < 10; i++)
@@ -61,7 +62,7 @@ public class HowmeWork7
             System.out.println(plate);
         }
         System.out.println("Надеюсь, всем хватило?");
-    
+
         System.out.println("\nПосмотрим, сыты ли коты?");
         for (int i = 0; i < 10; i++)
         {
@@ -75,60 +76,59 @@ class SmartCat
     private String name;
     private int appetite;
     private boolean hungry;
-    
+
     public SmartCat(String name, int appetite)
     {
         this.name = name;
         this.appetite = appetite;
         this.hungry = true;
     }
-    
+
     public boolean isHungry()
     {
         return hungry;
     }
-    
+
     public void setHungry(boolean hungry)
     {
         this.hungry = hungry;
     }
-    
+
     public String getName()
     {
         return name;
     }
-    
+
     public void setName(String name)
     {
         this.name = name;
     }
-    
+
     public int getAppetite()
     {
         return appetite;
     }
-    
+
     public void setAppetite(int appetite)
     {
         this.appetite = appetite;
     }
-    
+
     boolean eat(Plate plate)
     {
-        boolean RC = false;
+        boolean haveEatten = false;
         if (plate.getFood() >= appetite)
         {
             System.out.println(name + ": Хрум-хрум.");
             plate.decreaseFood(appetite);
-            RC = true;
-        }
-        else
+            haveEatten = true;
+        } else
         {
             System.out.println(name + ": Мяяяяу!!!");
         }
-        return RC;
+        return haveEatten;
     }
-    
+
     @Override
     public String toString()
     {
@@ -143,28 +143,28 @@ class SmartCat
 class Plate
 {
     private int food;
-    
+
     public Plate(int food)
     {
         this.food = food;
     }
-    
+
     void decreaseFood(int appetite)
     {
         this.food -= appetite;
     }
-    
+
     @Override
     public String toString()
     {
         return "Тарелка { еда = " + food + " }";
     }
-    
+
     public int getFood()
     {
         return food;
     }
-    
+
     public void setFood(int food)
     {
         this.food = food;
